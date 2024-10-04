b63ffb25217699fa501b94c1a286dab5c7810a69
Felipe de Freitas da Silva <163686237+ffelipef@users.noreply.github.com>
2024-10-03 23:47:16 -0300

    Teste Unitário MainTest.java

--- Falta organizar tabela excel e acrescentar testes  unitarios/proj2/src/test/java/com/example/proj2/MainTest.java
+++ Falta organizar tabela excel e acrescentar testes  unitarios/proj2/src/test/java/com/example/proj2/MainTest.java
@@ -25,7 +25,51 @@ public class MainTest {
         assertEquals(area.getNome(), "ABC");
         
         
+    }    
+    
+    @Test
+    public void testTipo() {
+        Especie especies = new Especie();
+        
+        especies.setTipo("teste");
+        
+        assertEquals(especies.getTipo(), "teste");
     }
     
     
+    @Test
+    public void testLatitude() {
+        Area area = new Area();
+        
+        area.setLatitude(2.0);
+        
+        assertEquals(area.getLatitude(), 2.0);
+    }
+    
+    @Test
+    public void testLongitude() {
+        Area area = new Area();
+        
+        area.setLongitude(4.0);
+        
+        assertEquals(area.getLongitude(), 4.0);
+    }
+    
+    @Test
+    public void testHectare() {
+        Area area = new Area();
+        
+        area.setHectares(15.0);
+        
+        assertEquals(area.getHectares(), 15.0);
+    }
+    
+    @Test
+    public void testCondicao(){
+        Especie especie = new Especie();
+        
+        especie.setCondicao("ameacada");
+        
+        assertEquals(especie.getCondicao(), "ameacada");
+    }
 }
